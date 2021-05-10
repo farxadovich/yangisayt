91eff27e7c351ce94b075b9e65f7df72b00dca41
farxadovich <abbosmahamedov4@gmail.com>
2021-05-10 12:12:47 +0500

    py

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,17 @@ from  . import views
 
 
 urlpatterns = [
+
     path('', views.index, name='index'),
-    path('second/', views.second)
+
+    path('second/', views.second),
+    path('uch/', views.uch, name='uch'),
+    path('tort/', views.tort, name='tort'),
+    path('besh/', views.besh, name='besh'),
+    path('olti/', views.olti, name='olti'),
+    path('yetti/', views.yetti, name='tetti'),
+    path('sakkiz/', views.sakkiz, name='sakkiz'),
+    path('toqqiz/', views.toqqiz, name='toqqiz'),
+    path('on/', views.on, name='on'),
+
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,8 +3,37 @@ from django.shortcuts import render, HttpResponse
 
 
 def index(request):
-    return HttpResponse('<a href="http://127.0.0.1:8000/second/">Salom Guruh!</a>')
+    return HttpResponse('<a href="http://127.0.0.1:8000/second/">Birinchi sayt</a><br><br><button>Next</button>')
 
 
 def second(request):
-    return HttpResponse('Xayr guruh!<button>Button</button>')
+    return HttpResponse('<a href="http://127.0.0.1:8000/uch/">ikkinchi sayt</a><br><br><button>Next</button>')
+
+
+def uch(request):
+    return HttpResponse('<a href="http://127.0.0.1:8000/tort/">Uchinchi sayt</a><br><br><button>Next</button>')
+
+
+
+def tort(request):
+    return HttpResponse('<a href="http://127.0.0.1:8000/besh/">Tort sayt</a><br><br><button>Next</button>')
+
+def besh(request):
+    return HttpResponse('<a href="http://127.0.0.1:8000/olti/">Besh sayt</a><br><br><button>Next</button>')
+
+def olti(request):
+    return HttpResponse('<a href="http://127.0.0.1:8000/yetti/">OLti sayt</a><br><br><button>Next</button>')
+
+def yetti(request):
+    return HttpResponse('<a href="http://127.0.0.1:8000/sakkiz/">Tetti sayt</a><br><br><button>Next</button>')
+
+
+def sakkiz(request):
+    return HttpResponse('<a href="http://127.0.0.1:8000/toqqiz/">Sakkiz sayt</a><br><br><button>Next</button>')
+
+def toqqiz(request):
+    return HttpResponse('<a href="http://127.0.0.1:8000/on">Toqqiz sayt</a><br><br><button>Next</button>')
+
+
+def on(request):
+    return HttpResponse('<a href="http://127.0.0.1:8000/">On sayt</a><br><br><button>Home</button>')
